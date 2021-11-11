e4bae7384c8878da89d720766ce6b4804f5ca286
Josh McArthur <joshua.mcarthur@gmail.com>
2021-11-12 11:40:16 +1300

    Build the AWS client init into the drop delegate so that the latest settings are picked up

--- Droplet/DropletDropDelegate.swift
+++ Droplet/DropletDropDelegate.swift
@@ -10,19 +10,31 @@ import SotoS3
 import AVFoundation
 
 struct DropletDropDelegate : DropDelegate {
+    @AppStorage("awsAccessKeyId") var awsAccessKeyId = ""
+    @AppStorage("awsSecretAccessKey") var awsSecretAccessKey = ""
+    @AppStorage("awsRegion") var awsRegion = "";
+    @AppStorage("awsBucketName") var awsBucketName = ""
+    @AppStorage("awsDefaultObjectExpiry") var awsDefaultObjectExpiry = ObjectExpiryOptions.one_day
+    
+    
     func validateDrop(info: DropInfo) -> Bool {
         return info.hasItemsConforming(to: ["public.file-url"])
     }
     
     
     func performDrop(info: DropInfo) -> Bool {
+        let awsClient = AWSClient(
+            credentialProvider: .static(accessKeyId: awsAccessKeyId, secretAccessKey: awsSecretAccessKey),
+            httpClientProvider: .createNew
+        )
+        let s3 = S3(client: awsClient, region: SotoS3.Region.init(rawValue: awsRegion));
         if let item = info.itemProviders(for: ["public.file-url"]).first {
                         item.loadItem(forTypeIdentifier: "public.file-url", options: nil) { (urlData, error) in
                             DispatchQueue.main.async {
                                 if let urlData = urlData as? Data {
                                     self.active = true
                                     self.fileUrl = NSURL(absoluteURLWithDataRepresentation: urlData, relativeTo: nil) as URL
-                                    let request = S3.CreateMultipartUploadRequest(bucket: AWS_BUCKET,
+                                    let request = S3.CreateMultipartUploadRequest(bucket: awsBucketName,
                                                                                   contentDisposition: "inline",
                                                                                   contentType: self.fileUrl!.mimeType(),
                                                                                   key: NSUUID().uuidString)
@@ -40,6 +52,12 @@ struct DropletDropDelegate : DropDelegate {
                                     multipartUploadRequest.whenFailure { error in
                                         self.active = false;
                                         print(error)
+
+                                        DispatchQueue.main.async {
+                                            NSApplication.shared.presentError(error)
+                                        }
+                                        try! awsClient.syncShutdown()
+
                                     }
                                     
                                     multipartUploadRequest.whenSuccess { output in
@@ -50,15 +68,17 @@ struct DropletDropDelegate : DropDelegate {
                                                 self.signedUrl = try s3.signURL(
                                                     url: URL(string: location)!,
                                                     httpMethod: .GET,
-                                                    expires: AWS_PRESIGNED_OBJECT_EXPIRY
+                                                    expires: SotoS3.TimeAmount.hours(Int64(awsDefaultObjectExpiry.rawValue))
                                                 ).wait()
                                                 
                                                 let pasteboard = NSPasteboard.general
                                                 pasteboard.clearContents()
                                                 pasteboard.setString(self.signedUrl!.absoluteString, forType: .string)
+                                                try! awsClient.syncShutdown()
                                                 self.active = false
                                             }
                                             catch {
+                                                NSApplication.shared.presentError(error)
                                                 print("Could not generated presigned URL for \(location)")
                                             }
                                         }
